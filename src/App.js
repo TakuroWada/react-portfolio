a3bf6dc8b5ff9f2f6499ffd58f5130af125d912a
takuro <takuro.wada6@gmail.com>
2020-11-17 21:52:00 +0900

    add プロフィールコンポーネントの表示 #8

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './assets/App.css';
 import Header from './components/molecules/Header'
 import FirstView from './components/molecules/FirstView'
+import Profile from './components/molecules/Profile'
+
 function App() {
   return (
     <div className="App">
       <header><Header /></header>
       <FirstView />
+      <Profile />
     </div>
   );
 }
