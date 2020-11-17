67d8c11b6bf9d767f93e140e8ad128b9c13cb7e6
takuro <takuro.wada6@gmail.com>
2020-11-17 17:54:26 +0900

    add FirstViewを作成 #6

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './assets/App.css';
 import Header from './components/molecules/Header'
+import FirstView from './components/molecules/FirstView'
 function App() {
   return (
     <div className="App">
       <header><Header /></header>
+      <FirstView />
     </div>
   );
 }
