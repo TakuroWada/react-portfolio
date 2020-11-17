04cd59daaaa3b0e5746576bd6d81fa53e3916abb
takuro <takuro.wada6@gmail.com>
2020-11-17 23:07:28 +0900

    add キャリアコンポーネントの表示 #10

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './assets/App.css';
 import Header from './components/molecules/Header'
 import FirstView from './components/molecules/FirstView'
 import Profile from './components/molecules/Profile'
+import Carear from './components/molecules/Carear'
+
 
 function App() {
   return (
@@ -9,6 +11,7 @@ function App() {
       <header><Header /></header>
       <FirstView />
       <Profile />
+      <Carear />
     </div>
   );
 }
