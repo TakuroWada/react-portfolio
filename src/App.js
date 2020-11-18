5dc06ce2561950fc5c014f7d4620d5b13e518a83
takuro <takuro.wada6@gmail.com>
2020-11-18 15:41:43 +0900

    add Contactコンポーネント表示 #20

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Carear from './components/molecules/Carear'
 import Skill from './components/molecules/Skill'
 import Learning from './components/molecules/Learning'
 import Work from './components/molecules/Work'
+import Contact from './components/molecules/Contact'
 
 
 function App() {
@@ -18,6 +19,7 @@ function App() {
       <Skill />
       <Learning />
       <Work />
+      <Contact />
     </div>
   );
 }
